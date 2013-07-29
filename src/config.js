eb1764a9f8b986b970a200d9ea34c304522874f9
Orestes Dominguez <orestes.dominguez@neodoo.es>
2013-07-29 15:26:55 +0200

    BUGFIX solved error with config paths

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 var config = {};
 
 config.port = process.env.PORT || 8000;
-config.engines_pah = process.env.ENGINES_PATH || './engines';
-config.game_store = './stores/game_store_mem.js';
-config.player_store = './stores/player_store_mem.js';
+config.engines_path = process.env.ENGINES_PATH || __dirname + '/engines';
+config.game_store = __dirname + '/stores/game_store_mem.js';
+config.player_store = __dirname + '/stores/player_store_mem.js';
 
 module.exports = config;
